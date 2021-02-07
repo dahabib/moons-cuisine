e856b028e2ffe43063670e978366c4a54e5ac360
Delwar Ahmed <delwar.habib@gmail.com>
2021-02-07 18:57:09 +0600

    fixed fetched data displaying issue

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const loadMeal = search => {
     console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => displayMeal(data));
+        .then(data => displayMeal(data.meals));
 }
 
 const searchButton = document.getElementById("search-button");
@@ -16,36 +16,20 @@ searchButton.addEventListener("click", () => {
 });
 
 const displayMeal = meals => {
-    console.log(meals);
-    const mealsDiv = document.getElementById('showcase-meal');
-    // meals.forEach(meal => {
-    //     const mealDiv = document.createElement('div');
-    //     mealDiv.className = 'single-meal';
-    //     const mealImage = meal.strMealThumb;
-    //     const mealDetails = `
-    //         <img src="${mealImage}" alt="Meal Image" id="meal-image">
-    //         <h3 class="meal-name">${meal.strMeal}</h3>
-    //         <button onclick="displayCountryDetail('${meal.strMeal}')">Details</button>
-    //     `;
-    //     mealDiv.innerHTML = mealDetails;
-    //     mealsDiv.appendChild(mealDiv);
-    // });
-    meals.forEach(function (meal) {
+    const mealsContainer = document.getElementById('showcase-meal');
+    meals.forEach(meal => {
+        console.log(meal);
         const mealDiv = document.createElement('div');
         mealDiv.className = 'single-meal';
         const mealImage = meal.strMealThumb;
         const mealDetails = `
             <img src="${mealImage}" alt="Meal Image" id="meal-image">
             <h3 class="meal-name">${meal.strMeal}</h3>
-            <button onclick="displayCountryDetail('${meal.strMeal}')">Details</button>
+            <button onclick="mealDetail('${meal.strMeal}')">Details</button>
         `;
         mealDiv.innerHTML = mealDetails;
-        mealsDiv.appendChild(mealDiv);
+        mealsContainer.appendChild(mealDiv);
     });
-
-    // const mealName = meals.meals[0].strMeal;
-    // document.getElementById('meal-name').innerText = mealName;
-    // const mealImage = meals.meals[0].strMealThumb;
-    // document.getElementById('meal-image').setAttribute("src", mealImage);
 }
-
+    
+// const mealDetail = 
